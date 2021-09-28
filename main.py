a79878f79f401f2641c68e3f798d2ce832d1d839
ESMA3000 <58027922+ESMA3000@users.noreply.github.com>
2021-09-28 13:43:02 +0200

    ignore

--- main.py
+++ main.py
@@ -12,6 +12,5 @@ async def queryLemma(docs:str):
 async def jsonLemma(docs:json):
     return {"docs": docs}
 
-if __name__ == "__main__":
-    uvicorn.run(app,host="0.0.0.0", port=5000)
+uvicorn.run(app,host="0.0.0.0", port=5000)
 
