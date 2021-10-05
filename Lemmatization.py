44e6c65e9a77404477052d509ef2711f82bd7ee9
Kasper Masznicz <kmaszn19@student.aau.dk>
2021-10-05 10:50:47 +0200

    added danish and english support through API

--- Lemmatization.py
+++ Lemmatization.py
@@ -1,17 +1,21 @@
 import en_core_web_sm
 import da_core_news_sm
-import json
-
-#jsonTest= {"Title:":"Lol","Year":"2013","Text":"This is a test for the lemmatizer"}
 
 def danishLemmatization(string):
+
     danLemma = da_core_news_sm.load()
     doc = danLemma(string)
 
     return " ".join([token.lemma_ for token in doc])
 
 def englishLemmatization(string):
+
     engLemma = en_core_web_sm.load()
     doc = engLemma(string)
 
     return " ".join([token.lemma_ for token in doc])
+
+def Lemmatization(string, language):
+    
+    if language == "da":return danishLemmatization(string)
+    elif language == "en":return englishLemmatization(string)
--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 import Lemmatization as lemma
-import json
 
 app = FastAPI()
 
-@app.get("/lemma/{docs}")
-async def queryLemma(docs:str):
-    return {"docs": lemma.danishLemmatization(docs)}
-
-async def jsonLemma(docs:json):
-    return {"docs": docs}
+@app.get("/lemma/{language}/{docs}")
+async def queryLemma(docs:str,language:str):
+    return {"docs": lemma.Lemmatization(docs,language)}
 
 if __name__=="__main__":
-    print("hej")
     uvicorn.run(app,host="0.0.0.0", port=5000)
-
