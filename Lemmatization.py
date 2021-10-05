fe5de9c493f5a31cf40075b313f2981bea54767d
Kasper Masznicz <kmaszn19@student.aau.dk>
2021-10-05 12:17:57 +0200

    clean up and added comments

--- Lemmatization.py
+++ Lemmatization.py
@@ -1,21 +1,22 @@
+# Imported from spacy.io
 import en_core_web_sm
 import da_core_news_sm
 
 def danishLemmatization(string):
-
+    # Load the core module
     danLemma = da_core_news_sm.load()
+    # Parse the string
     doc = danLemma(string)
-
+    # Extract the lemma for each token and join the lemma with a space between every word. Then return it
     return " ".join([token.lemma_ for token in doc])
 
 def englishLemmatization(string):
-
+    # Same as in danishLemmatization()
     engLemma = en_core_web_sm.load()
     doc = engLemma(string)
-
     return " ".join([token.lemma_ for token in doc])
 
 def Lemmatization(string, language):
-    
+    # Check which language to lemmatize and return the lemmatization
     if language == "da":return danishLemmatization(string)
     elif language == "en":return englishLemmatization(string)
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import uvicorn
 from fastapi import FastAPI
+import uvicorn
 import Lemmatization as lemma
 
+# Create an instance of FastAPI
 app = FastAPI()
 
-@app.get("/lemma/{language}/{docs}")
-async def queryLemma(docs:str,language:str):
-    return {"docs": lemma.Lemmatization(docs,language)}
+# Handles the path and the GET request 
+# The parameters needed is the language and text that is going to be lemmatized
+@app.get("/lemma/{language}/{text}")
+# Function that returns the lemmatized text
+async def queryLemma(text:str,language:str):
+    return {"lemmatized_text": lemma.Lemmatization(text,language)}
 
+# uvicorn controls which host and port the API is available at.
 if __name__=="__main__":
     uvicorn.run(app,host="0.0.0.0", port=5000)
