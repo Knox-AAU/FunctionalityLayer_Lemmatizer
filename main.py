061570e84b63464876aa6944f5f5212b3004076b
Kasper Masznicz <kmaszn19@student.aau.dk>
2021-10-13 11:06:23 +0200

    added new path queries

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 
 # Handles the path and the GET request 
 # The parameters needed is the language and text that is going to be lemmatized
-@app.get("/lemma/{language}/{text}")
+@app.get("/lemma/")
 # Function that returns the lemmatized text
-async def queryLemma(text:str,language:str):
+async def queryLemma(text:str="",language:str="da"):
     return {"lemmatized_text": lemma.Lemmatization(text,language)}
 
 # uvicorn controls which host and port the API is available at.
