bfeb58ede38db2eed059c2a2db76028ea762ccc9
Schwartz123 <70626941+Schwartz123@users.noreply.github.com>
2022-11-10 11:07:59 +0100

    Small changes to language
    made it so it more moves the loaded language instead of having more if statements

--- token_maker.py
+++ token_maker.py
@@ -6,25 +6,27 @@ import en_core_web_sm
 
 def lemmatization(string, language):
     if language == "da":
-        return get_trimmed_words(get_tokens(string), "da")
-
-    if language == "en":
-        return get_trimmed_words(get_tokens(string), "en")
-
-    temp_language = da_core_news_sm.load()
-    Language.factory("language_detector", func=get_lang_detector)
-    temp_language.add_pipe("language_detector")
-    doc = temp_language(string)
-    input_language = doc._.language["language"]
-    if input_language in ('en', 'da'):
-        return get_trimmed_words(get_tokens(string), input_language)
-    return "Text is not of a supported language"
-
-def get_trimmed_words(words, language):
-    if language == "da":
         nlp = da_core_news_sm.load()
+        return get_trimmed_words(get_tokens(string), nlp)
     elif language == "en":
         nlp = en_core_web_sm.load()
+        return get_trimmed_words(get_tokens(string), nlp)
+    else:
+        temp_language = da_core_news_sm.load()
+        Language.factory("language_detector", func=get_lang_detector)
+        temp_language.add_pipe("language_detector")
+        doc = temp_language(string)
+        input_language = doc._.language["language"]
+        if input_language == 'da':
+            nlp = da_core_news_sm.load()
+            return get_trimmed_words(get_tokens(string), nlp)
+        if input_language == 'en':
+            nlp = en_core_web_sm.load()
+            return get_trimmed_words(get_tokens(string), nlp)
+
+    return "Text is not of a supported language"
+
+def get_trimmed_words(words, nlp):
     words_str = ' '.join(words)
     doc = nlp(words_str)
     word_lemmas = []
@@ -46,9 +48,9 @@ def get_token_re_pattern():
 
 def get_language(string):
     lemma = da_core_news_sm.load()
+    dected_language = "da"
     Language.factory("language_detector", func=get_lang_detector)
     lemma.add_pipe("language_detector")
-    dected_language = "da"
     doc = lemma(string.lower())
     if doc._.language["language"] == "en":
         lemma = en_core_web_sm.load()
