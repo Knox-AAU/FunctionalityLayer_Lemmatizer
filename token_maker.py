5118c45a6a5f824158d351789ff7198ee6357402
Schwartz123 <70626941+Schwartz123@users.noreply.github.com>
2022-11-17 11:40:17 +0100

    Removed bad smell
    made it so we don't have some duplicated code

--- token_maker.py
+++ token_maker.py
@@ -4,27 +4,17 @@ from spacy_language_detection import LanguageDetector
 import da_core_news_sm
 import en_core_web_sm
 
-def lemmatization(string, language):
+def lemmatization(input_str, language):
+    if language is None:
+        language = get_language(input_str)
     if language == "da":
         nlp = da_core_news_sm.load()
-        return get_trimmed_words(get_tokens(string), nlp)
+        return get_trimmed_words(get_tokens(input_str), nlp)
     elif language == "en":
         nlp = en_core_web_sm.load()
-        return get_trimmed_words(get_tokens(string), nlp)
+        return get_trimmed_words(get_tokens(input_str), nlp)
     else:
-        temp_language = da_core_news_sm.load()
-        Language.factory("language_detector", func=get_lang_detector)
-        temp_language.add_pipe("language_detector")
-        doc = temp_language(string)
-        input_language = doc._.language["language"]
-        if input_language == 'da':
-            nlp = da_core_news_sm.load()
-            return get_trimmed_words(get_tokens(string), nlp)
-        if input_language == 'en':
-            nlp = en_core_web_sm.load()
-            return get_trimmed_words(get_tokens(string), nlp)
-
-    return "Text is not of a supported language"
+        return "Text is not of a supported language"
 
 def get_trimmed_words(words, nlp):
     words_str = ' '.join(words)
@@ -46,18 +36,19 @@ def get_tokens(text):
 def get_token_re_pattern():
     return re.compile(r'[0-9]+,[0-9]+|[a-z0-9æøå]{2,}|[0-9]+')
 
-def get_language(string):
+def get_language(input_str):
     lemma = da_core_news_sm.load()
     dected_language = "da"
     Language.factory("language_detector", func=get_lang_detector)
     lemma.add_pipe("language_detector")
-    doc = lemma(string.lower())
+    doc = lemma(input_str.lower())
     if doc._.language["language"] == "en":
         lemma = en_core_web_sm.load()
-        doc = lemma(string)
+        doc = lemma(input_str)
         dected_language = "en"
         return dected_language
-    return "Language is not supported"
+    else:
+        return "Language is not supported"
 
 def get_lang_detector(nlp, name):
     return LanguageDetector()
